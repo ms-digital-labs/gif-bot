8430401463b8283264c802a1ce86dc96492e4add
Pip Taylor <pip@evilgeek.co.uk>
2014-07-23 11:14:58 +0100

    trim isn't a method on Ruby strings. Use strip.
    I've been writing too much Javascript recently.

--- gif_bot.rb
+++ gif_bot.rb
@@ -3,7 +3,7 @@ require "json"
 
 class GifBot < Sinatra::Base
   post "/" do
-    search_term = params[:text].gsub(params[:trigger_word], "").trim
+    search_term = params[:text].gsub(params[:trigger_word], "").strip
     content_type :json
     { text: search_term }.to_json
   end
