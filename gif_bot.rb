f677ce7b86eff3bf3a821e6b6ccd532cd1bd8f45
Pip Taylor <pip@evilgeek.co.uk>
2014-07-23 11:29:44 +0100

    Check for required parameters.

--- gif_bot.rb
+++ gif_bot.rb
@@ -3,11 +3,20 @@ require "json"
 
 class GifBot < Sinatra::Base
   post "/" do
+    return error("I need text and trigger_word parameters") unless params_present?
     search_term = params[:text].gsub(params[:trigger_word], "").strip
     gifs = Giphy.search(search_term)
     random_gif = gifs.sample
 
     content_type :json
     { text: random_gif.original_image.url }.to_json
+  end
+
+  def params_present?
+    params.has_key?("text") && params.has_key?("trigger_word")
+  end
+
+  def error(error_message)
+    { text: error_message }.to_json
   end
 end
